db93ba45aa67acaa2364addc67ad02060f4552aa
Nirupa <nirupasinha4@gmail.com>
2021-07-03 22:10:24 +0530

    new branch => default rout

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 module.exports = {
     appRoutes: (app) => {
         const v1 = express.Router();
+
+        function hello(req, res) {
+            console.log("hello")
+            res.send("Hello Sarvottam")
+
+        }
+        v1.get("/", hello)
         app.use('/api', v1);
         v1.use('/auth', require('./authRoutes'));
         v1.use('/user', require('./userRoutes'));
